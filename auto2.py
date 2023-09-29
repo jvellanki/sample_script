4df69c2351dda29a718e599a75bc81306c015082
jvellanki <144234294+jvellanki@users.noreply.github.com>
2023-09-29 17:02:29 +0530

    Add files via upload

---
+++ auto2.py
@@ -0,0 +1,31 @@
+import pyautogui
+import subprocess
+import time
+
+# Open Notepad++
+subprocess.Popen(['notepad++.exe'])
+
+# Wait for Notepad++ to open
+time.sleep(2)
+
+# Define the message you want to type
+message = "Hello, Notepad++ Automation!"
+
+# Type the message into Notepad++
+pyautogui.typewrite(message)
+
+# Save the file (you can modify the path and filename)
+pyautogui.hotkey('ctrl', 's')
+
+# Wait for the "Save As" dialog to appear (you can adjust the duration)
+time.sleep(2)
+
+# Type the file name
+filename = "automated_message.txt"
+pyautogui.typewrite(filename)
+
+# Press Enter to save the file
+pyautogui.press('enter')
+
+# Close Notepad++
+pyautogui.hotkey('alt', 'f4') 
