1d920fb0fa26b814b91561b31cc0c1196ac340ff
jvellanki <144234294+jvellanki@users.noreply.github.com>
2023-09-29 19:27:07 +0530

    Add files via upload

---
+++ auto4.py
@@ -0,0 +1,36 @@
+import subprocess
+import time
+import pyautogui
+
+# Path to the Windows Calculator executable
+calculator_exe = "calc.exe"
+
+try:
+    # Open Windows Calculator
+    subprocess.Popen(calculator_exe)
+    print("Windows Calculator is now open.")
+
+    # Wait for Calculator to open
+    time.sleep(2)
+
+    # Perform basic addition (e.g., 5 + 3)
+    pyautogui.typewrite("5")
+    pyautogui.press("plus")
+    pyautogui.typewrite("3")
+    pyautogui.press("enter")
+
+    # Wait for the result to appear
+    time.sleep(1)
+
+    # Capture the result
+    result = pyautogui.screenshot(region=(500, 250, 200, 100))  # Adjust the region as needed
+
+    # Save the result as an image (optional)
+    result.save("calculator_result.png")
+    print("Screenshot saved as 'calculator_result.png'.")
+
+except FileNotFoundError:
+    print("Error: The Windows Calculator executable was not found.")
+
+except Exception as e:
+    print(f"An error occurred: {e}")
