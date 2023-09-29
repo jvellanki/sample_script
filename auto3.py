85452861ac4b19726ee21eb216055b81d0698f36
jvellanki <144234294+jvellanki@users.noreply.github.com>
2023-09-29 19:01:09 +0530

    Add files via upload

---
+++ auto3.py
@@ -0,0 +1,15 @@
+import subprocess
+
+# Path to the Windows Calculator executable
+calculator_exe = "calc.exe"
+
+try:
+    # Open Windows Calculator
+    subprocess.Popen(calculator_exe)
+    print("Windows Calculator is now open.")
+
+except FileNotFoundError:
+    print("Error: The Windows Calculator executable was not found.")
+
+except Exception as e:
+    print(f"An error occurred: {e}") 
